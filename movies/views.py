834e3f625384f96d626a090d5bda8da04732491f
kurogogyou <ml.vizard@gmail.com>
2019-08-24 09:05:12 -0400

    Added Movies App and ugly Hello World homepage

---
+++ movies/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib import messages
+from airtable import Airtable
+import os
+
+# Create your views here.
+
+def home_page(request):
+	return render(request, 'movies/movies_stuff.html')
