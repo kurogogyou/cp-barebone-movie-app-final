7c547974a4c4360f6e265f4e7f2dfb52084c5ee4
kurogogyou <ml.vizard@gmail.com>
2019-08-25 08:42:27 -0400

    Regenerated the virtual env with Pipenv into the project folder and managed to export the variables when hosting locally, to create the airtable handler correctly.

--- host_locally.sh
+++ host_locally.sh
@@ -1,11 +1,26 @@
-#!/bin/bash
 
-# Converted into unix newline endings by dos2unix linux command. This script is used because normal users have no
+# Converted into unix newline endings by dos2unix linux command. 
+
+# This script is used because normal users have no
 # access to hosting on external ports on the VM. This way you can host a page in the (W)LAN.
 
-# This is the default pipenv environment within the project:
-source /home/mario/devenvs/python/cp-barebone-movie-app-final/bin/activate
+# If no powers, re-exec the script with powers
+#[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
+#(( EUID != 0 )) && exec sudo -- "$0" "$@"
+
 # This is the project folder
 cd /home/mario/devenvs/python/cp-barebone-movie-app-final/
+
+# This adds the environment variables into the actual environment
+# direnv allow .
+for V in $(cat .env)
+do    
+	export $V 
+done
+
+# This is the default pipenv environment within the project:
+# /home/mario/devenvs/python/cp-barebone-movie-app-final/
+source .venv/bin/activate
+
 # This command runs the locally hosted server
 python3 manage.py runserver 0.0.0.0:80
--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.contrib import messages
-#from airtable import airtable
+from airtable.airtable import Airtable
 import os
 
 # Create your views here.
 
-def home_page(request):
-	return render(request, 'movies/movies_stuff.html')
+# Airtable database handler
+AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID'), 'Movies') #api_key=os.environ.get('AIRTABLE_API_KEY')
 
 # This runs when soembody comes to my website
+def home_page(request):
+	#print(os.environ.get('AIRTABLE_API_KEY'))
+	#print(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID'))
+	#print(str(request.GET.get('query', '')))
+	user_query = str(request.GET.get('query', ''))
+	return render(request, 'movies/movies_stuff.html')
+
+
