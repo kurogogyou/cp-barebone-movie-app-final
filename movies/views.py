cf59f5b0a7727dc7d8c6aeaaee5149266e584dde
kurogogyou <ml.vizard@gmail.com>
2019-09-03 16:40:39 -0400

    Made the app error resistant,required inputs, creating, editing and deleting records. Shows errors as messages.

--- movies/views.py
+++ movies/views.py
@@ -5,6 +5,8 @@ import os
 
 # Create your views here.
 
+# https://www.genesisglobalschool.edu.in/wp-content/uploads/2016/09/noimage.jpg
+
 # Airtable database handler
 AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID'), 'Movies', api_key=os.environ.get('AIRTABLE_API_KEY'))
 
@@ -20,38 +22,49 @@ def home_page(request):
 
 def create(request):
 	if request.method == 'POST':
-		data = {
-			'Name': request.POST.get('name'),
-			'Pictures': [{'url': request.POST.get('url')}],
-			'Rating': int(request.POST.get('rating')),
-			'Notes': request.POST.get('notes')
-		}
-
-		response = AT.insert(data)
-		#response['fields'] is a dictionary too.
-		messages.success(request, 'New movie added: {}'.format(response['fields'].get('Name')))
+		try:
+			data = {
+				'Name': request.POST.get('name'),
+				# Add default No Image picture if none was provided.
+				'Pictures' : [{'url' : request.POST.get('url') or 'https://www.genesisglobalschool.edu.in/wp-content/uploads/2016/09/noimage.jpg'}],
+				'Rating': int(request.POST.get('rating')),
+				'Notes': request.POST.get('notes')
+			}
+			
+			response = AT.insert(data)
+			#response['fields'] is a dictionary too.
+			messages.success(request, 'New movie added: {}'.format(response['fields'].get('Name')))
+		except Exception as e:
+			messages.warning(request, 'Error when creating: {}'.format(e))
 
 	return redirect('/')
 
 def edit(request, movie_id):
 	if request.method == 'POST':
-		data = {
-			'Name' : request.POST.get('name'),
-			'Pictures' : [{'url' : request.POST.get('url')}],
-			'Rating' : int(request.POST.get('rating')),
-			#'Notes' : request.POST.get('notes')
-		}
-		#Only update the notes when the string is not empty. Prevents automatically removing notes.
-		if request.POST.get('notes') != '':
-			data.update({'Notes' : request.POST.get('notes')})
+		try:
+			data = {
+				'Name' : request.POST.get('name'),
+				'Pictures' : [{'url' : request.POST.get('url')}],
+				'Rating' : int(request.POST.get('rating')),
+				#'Notes' : request.POST.get('notes')
+			}
+			#Only update the notes when the string is not empty. Prevents automatically removing notes.
+			if request.POST.get('notes') != '':
+				data.update({'Notes' : request.POST.get('notes')})
 
-		response = AT.update(movie_id, data)
-		messages.success(request, 'Edited movie: {}'.format(response['fields'].get('Name')))
+		
+			response = AT.update(movie_id, data)
+			messages.success(request, 'Edited movie: {}'.format(response['fields'].get('Name')))
+		except Exception as e:
+			messages.warning(request, 'Error when updating: {}'.format(e))
 	return redirect('/')
 
 def delete(request, movie_id):
-	#Need to get the name of the movie before I delete it.
-	name = AT.get(movie_id)['fields'].get('Name')
-	AT.delete(movie_id)
-	messages.warning(request, 'Deleted movie: {}'.format(name))
+	try:
+		#Need to get the name of the movie before I delete it.
+		name = AT.get(movie_id)['fields'].get('Name')
+		AT.delete(movie_id)
+		messages.warning(request, 'Deleted movie: {}'.format(name))
+	except Exception as e:
+		messages.warning(request, 'Error when deleting: {}'.format(e))
 	return redirect('/')
